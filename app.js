8096ed046229c400efed3bc8a24d01195a23ad33
Rob Ranf <rob@emiyaconsulting.com>
2022-06-16 14:28:09 -0700

    adding shalltear and bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
-	res.send("Hello, World!");
+	res.sendFile(path.join(__dirname + "/public/html/home.html"));
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- ui/README.md
+++
@@ -1,1 +0,0 @@
-#This directory content is pending
