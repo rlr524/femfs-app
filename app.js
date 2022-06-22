9d8195a076e0a4c11c376aad8f4a461359ff6168
Rob Ranf <rob@emiyaconsulting.com>
2022-06-22 10:13:55 -0700

    custom header

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const port = 3000;
 const path = require("path");
 const winston = require("winston");
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 const logger = winston.createLogger({
 	level: "info",
 	format: winston.format.json(),
-	defaultMeta: { service: "user-service"},
+	defaultMeta: { service: "user-service" },
 	transports: [
-		new winston.transports.File({filename: "error.log", level: "error"}),
-		new winston.transports.File({filename: "combined.log"}),
+		new winston.transports.File({ filename: "error.log", level: "error" }),
+		new winston.transports.File({ filename: "combined.log" }),
 	],
 });
 
 app.get("/", (req, res) => {
 	try {
-		res.status(418);
-		res.header("Omae wa mou shindeiru");
+		res.header("x-omae-wa-mou-shindeiru", "Nani!!??");
+		// res.status(418)
 		res.sendFile(path.join(__dirname + "/public/html/home.html"));
 	} catch {
 		logger.error("Error: There was an error retrieving the page '/'");
@@ -29,5 +29,5 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`);
-	logger.info(`App listening on port ${port}`)
-})
+	logger.info(`App listening on port ${port}`);
+});
